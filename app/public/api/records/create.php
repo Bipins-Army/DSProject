7ce7bba6d10273129aac6c4f6e04b9e88ddb79c7
bradscot <bradenscot13@gmail.com>
2020-10-14 14:20:07 -0400

    create new php file

---
+++ app/public/api/records/create.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'common.php';
+
+// Only need this line if we're creating GUIDs (see comments below)
+
+// Step 1: Get a datase connection from our helper class
+$db = DbConnection::getConnection();
+
+// Step 2: Create & run the query
+// Note the use of parameterized statements to avoid injection
+$stmt = $db->prepare(
+  'INSERT INTO Patient (patientGuid, firstName, lastName, dob, sexAtBirth)
+  VALUES (?, ?, ?, ?, ?)'
+);
+
+$stmt->execute([
+  $guid,
+  $_POST['firstName'],
+  $_POST['lastName'],
+  $_POST['dob'],
+  $_POST['sexAtBirth']
+]);
+
+// If needed, get auto-generated PK from DB
+// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+
+// Step 4: Output
+// Here, instead of giving output, I'm redirecting to the SELECT API,
+// just in case the data changed by entering it
+header('HTTP/1.1 303 See Other');
+header('Location: ../records/?guid=' . $guid);
