1c6c9c4494d7262aeb0e20305806deac0d48a690
Alexis Yannuzzi <yannuzzi367@gmail.com>
2020-10-14 20:16:57 -0400

    updates

--- DS-Project/app/public/js/index.app.js
+++ DS-Project/app/public/js/index.app.js
@@ -1,61 +1,75 @@
 var app = new Vue({
-  el: '#triagePage',
+  el: '#memberPage',
   data: {
-    ptList: [],
-    activePt: null,
-    triageForm: {
-      priority: null,
-      symptoms: ''
-    },
-    newPtForm: {}
-  },
-  computed: {
-    activePtName() {
-      return this.activePt ? this.activePt.lastName + ', ' + this.activePt.firstName : ''
-    }
+    memberList: [],
+    activeMember: null,
+    memberForm: {},
+    newMemberForm: {}
   },
   methods: {
-    newPtData() {
+    newMemberData() {
       return {
         firstName: "",
         lastName: "",
-        dob: "",
+        street: "",
+        city: "",
+        state: "",
+        zipcode: "",
+        email: "",
+        dateofBirth: "",
+        startDate: "",
+        gender: "",
+        position: "",
+        radioNumber: "",
+        stationNumber: "",
+        isActive: "",
         sexAtBirth: ""
       }
     },
-    handleNewPtForm( evt ){
-      evt.preventDefault();  // Redundant w/ Vue's submit.prevent
-      /*
-      //TODO: Hook to API
-      fetch( url, {
-       method: "post",
-       data: data
-      })
-      */
+    handleNewMemberForm( evt ){
+      //evt.preventDefault();  // Redundant w/ Vue's submit.prevent
+    fetch('api/records/post.php', {
+          method:'POST',
+          body: JSON.stringify(this.newMemberForm),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => {
+          console.log("Returned from post:", json);
+          // TODO: test a result was returned!
+          this.memberList.push(json[0]);
+          this.newMemberForm = this.newMemberData();
+        });
 
-      console.log("Creating...!");
-      console.log(this.newPtForm);
+        console.log("Creating (POSTing)...!");
+        console.log(this.newMemberForm);
+      },
+      handleMemberForm( evt ) {
+        console.log("Member form submitted!");
 
-      this.ptList.push(this.newPtForm);
+        if (!this.activeMember) {
+          alert("ERROR: No member selected!");
+          return false;
+        }
+        this.memberForm.PersonalID = this.activeMember.PersonalID;
 
-      this.newPtForm = this.newPtData();
+        console.log(this.memberForm);
+
+        fetch('api/records/create.php', {
+          method:'POST',
+          body: JSON.stringify(this.memberForm),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+        .then( response => response.json() )
+        .then( json => {
+          console.log("Returned from member create:", json);
+          this.memberList = json;
+          this.newMemberForm = this.newMemberData();
+        });
+      }
     },
-    handleTriageForm( evt ) {
-      console.log("Form submitted!");
-
-      this.triageForm.pt = this.activePt;
-      console.log(this.triageForm);
-
-    }
-  },
-  created() {
-    fetch("dummy/pt-list.php")
-    .then( response => response.json() )
-    .then( json => {
-      this.ptList = json;
-
-      console.log(json)}
-    );
-    this.newPtForm = this.newPtData();
-  }
 })
--- app/public/api/records/create.php
+++ app/public/api/records/create.php
@@ -2,8 +2,6 @@
 
 require 'common.php';
 
-// Only need this line if we're creating GUIDs (see comments below)
-
 // Step 1: Get a datase connection from our helper class
 $db = DbConnection::getConnection();
 
@@ -12,24 +10,24 @@ $db = DbConnection::getConnection();
 $stmt = $db->prepare(
   'INSERT INTO Person (firstName, lastName, street, city, state, zipcode, email, dateofBirth, startDate, gender, position, radioNumber, stationNumber, isActive)
   VALUES
-  ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);'
+  ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
 );
 
 $stmt->execute([
-  $_POST['lastName'],
-  $_POST['dob'],
-  $_POST['sexAtBirth'],
   $_POST['firstName'],
   $_POST['lastName'],
-  $_POST['dob'],
-  $_POST['sexAtBirth'],
-  $_POST['firstName'],
-  $_POST['lastName'],
-  $_POST['dob'],
-  $_POST['sexAtBirth'],
-  $_POST['firstName'],
-  $_POST['lastName'],
-  $_POST['dob']
+  $_POST['street'],
+  $_POST['city'],
+  $_POST['state'],
+  $_POST['zipcode'],
+  $_POST['email'],
+  $_POST['dateofBirth'],
+  $_POST['startDate'],
+  $_POST['gender'],
+  $_POST['position'],
+  $_POST['radioNumber'],
+  $_POST['stationNumber'],
+  $_POST['isActive']
 ]);
 
 // If needed, get auto-generated PK from DB
@@ -39,4 +37,4 @@ $stmt->execute([
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
 header('HTTP/1.1 303 See Other');
-header('Location: ../records/?guid=' . $guid);
+header('Location: ../records/');
