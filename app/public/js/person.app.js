42b919fc16c6c0aeb4c8633f3d3000fe13134b11
bradscot <bradenscot13@gmail.com>
2020-10-15 22:17:42 -0400

    Added a new php page for person

---
+++ app/public/js/person.app.js
@@ -0,0 +1,55 @@
+var app = new Vue({
+  el: '#certificationPage',
+  data: {
+    certification: [{
+      certifyingAgency: "",
+      certificationName: "",
+      standardExpiry: "",
+    }],
+    newCertification: [{
+      certifyingAgency: "",
+      certificationName: "",
+      standardExpiry: "",
+    }],
+  },
+
+  methods: {
+    fetchCertification() {
+      fetch('api/Certification/')
+      .then(response => response.json())
+      .then(json => {
+        this.certification=json;
+        console.log(this.certification);
+      });
+    },
+    createCertification(){
+      //make a line for getting the // ID
+      fetch('api/Certification/create2.php',{
+      method:'POST',
+      body: JSON.stringify(this.newUser),
+      headers: {
+        "CONTENT_TYPE": "application/json; charset=utf-8"
+      }}
+    )
+    .then( response => response.json())
+    .then( json => {
+      console.log("Returned from post:", json);
+      this.certification.push(json[0]);
+      this.newCertification = this.newCertificationData();
+    });
+    console.log("Creating (POSTING)...!");
+    console.log(this.newCertification);
+  },
+  newCertificationData() {
+    return {
+      certifyingAgency: "",
+      certificationName: "",
+      standardExpiry: "",
+    }
+  }
+},
+created() {
+  this.fetchCertification();
+}
+}
+);
