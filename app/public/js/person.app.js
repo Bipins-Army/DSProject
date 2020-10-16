30cb4e6075d2e2a09ab84cefefb5e2dc7d49fb55
bradscot <bradenscot13@gmail.com>
2020-10-16 07:55:37 -0400

    i will scream if this works

--- app/public/js/person.app.js
+++ app/public/js/person.app.js
@@ -30,34 +30,34 @@ var app = new Vue({
         console.log(this.persons);
       });
     },
-    createPerson(){
-      this.newPerson.PersonalID = (this.newPerson.firstName.substring(0,1,2,3,4,5,6,7,8,9,10,11,12)+this.newPerson.lastName++this.newPerson.lastName).toLowerCase();
-      fetch('api/person/create_person.php',{
-      method:'POST',
-      body: JSON.stringify(this.newPerson),
-      headers: {
-        "CONTENT_TYPE": "application/json; charset=utf-8"
-      }}
-    )
-    .then( response => response.json())
-    .then( json => {
-      console.log("Returned from post:", json);
-      this.person.push(json[0]);
-      this.newPerson = this.newPersonData();
-    });
-    console.log("Creating (POSTING)...!");
-    console.log(this.newPerson);
-  },
-  newPersonData() {
-    return {
-      certifyingAgency: "",
-      certificationName: "",
-      standardExpiry: "",
-    }
-  }
-},
+//    createPerson(){
+//      this.newPerson.PersonalID = (this.newPerson.firstName.substring(0,1,2,3,4,5,6,7,8,9,10,11,12)+this.newPerson.lastName++this.newPerson.lastName).toLowerCase();
+//      fetch('api/person/create_person.php',{
+//      method:'POST',
+//      body: JSON.stringify(this.newPerson),
+//      headers: {
+//        "CONTENT_TYPE": "application/json; charset=utf-8"
+//      }}
+//    )
+//    .then( response => response.json())
+//    .then( json => {
+//      console.log("Returned from post:", json);
+  //    this.person.push(json[0]);
+  //    this.newPerson = this.newPersonData();
+//    });
+//    console.log("Creating (POSTING)...!");
+  //  console.log(this.newPerson);
+//  },
+//  newPersonData() {
+  //  return {
+  //    certifyingAgency: "",
+    //  certificationName: "",
+  //    standardExpiry: "",
+  //  }
+//  }
+//},
 created() {
-  this.fetchCertification();
+  this.fetchPerson();
 }
 }
-);
+});
