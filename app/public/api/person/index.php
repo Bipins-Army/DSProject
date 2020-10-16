7c48366c02bef63fb3fcb469dfe464149414a157
bradscot <bradenscot13@gmail.com>
2020-10-16 07:49:59 -0400

    haha

--- app/public/api/person/index.php
+++ app/public/api/person/index.php
@@ -1,28 +1,20 @@
 <?php
 
-//require 'common.php';
+require 'common.php';
 
-// Step 1: Get a datase connection from our helper class
-//$db = DbConnection::getConnection();
 
-// Step 2: Create & run the query
-//$sql = 'SELECT * FROM Person';
-//$vars = [];
+$db = DbConnection::getConnection();
 
-// if (isset($_GET['guid'])) {
-//   // This is an example of a parameterized query
-//   $sql = 'SELECT * FROM Patient WHERE patientGuid = ?';
-//   $vars = [ $_GET['guid'] ];
-// }
+$sql = 'SELECT * FROM Person';
+$vars = [];
 
-//$stmt = $db->prepare($sql);
-//$stmt->execute($vars);
 
-//$patients = $stmt->fetchAll();
+$stmt = $db->prepare($sql);
+$stmt->execute($vars);
 
-// Step 3: Convert to JSON
-//$json = json_encode($persons, JSON_PRETTY_PRINT);
+$persons = $stmt->fetchAll();
 
-// Step 4: Output
-//header('Content-Type: application/json');
-//echo $json;
+$json = json_encode($persons, JSON_PRETTY_PRINT);
+
+header('Content-Type: application/json');
+echo $json;
