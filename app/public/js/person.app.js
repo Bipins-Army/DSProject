18fc8ce5db67f0c25c36b8a7fb1db857bbeafab7
bradscot <bradenscot13@gmail.com>
2020-10-16 10:02:50 -0400

    edits

--- app/public/js/person.app.js
+++ app/public/js/person.app.js
@@ -29,33 +29,45 @@ var app = new Vue({
         this.persons=json;
         console.log(this.persons);
       });
+    },
+    createPerson(){
+      this.newPerson.PersonalID = (this.newPerson.firstName.substring(0,1,2,3,4,5,6,7,8,9,10,11,12)+this.newPerson.lastName++this.newPerson.lastName).toLowerCase();
+      fetch('api/person/create_person.php',{
+      method:'POST',
+      body: JSON.stringify(this.newPerson),
+      headers: {
+        "CONTENT_TYPE": "application/json; charset=utf-8"
+      }}
+    )
+    .then( response => response.json())
+    .then( json => {
+      console.log("Returned from post:", json);
+      this.person.push(json[0]);
+     this.newPerson = this.newPersonData();
+    });
+    console.log("Creating (POSTING)...!");
+    console.log(this.newPerson);
+  },
+  newPersonData() {
+    return {
+      personalID:'',
+      firstName:'',
+      lastName:'',
+      street:'',
+      city:'',
+      state:'',
+      zipcode:'',
+      email:'',
+      dateofBirth:'',
+      startDate:'',
+      gender:'',
+      position:'',
+      radioNumber:'',
+      stationNumber:'',
+      isActive:''
     }
-//    createPerson(){
-//      this.newPerson.PersonalID = (this.newPerson.firstName.substring(0,1,2,3,4,5,6,7,8,9,10,11,12)+this.newPerson.lastName++this.newPerson.lastName).toLowerCase();
-//      fetch('api/person/create_person.php',{
-//      method:'POST',
-//      body: JSON.stringify(this.newPerson),
-//      headers: {
-//        "CONTENT_TYPE": "application/json; charset=utf-8"
-//      }}
-//    )
-//    .then( response => response.json())
-//    .then( json => {
-//      console.log("Returned from post:", json);
-  //    this.person.push(json[0]);
-  //    this.newPerson = this.newPersonData();
-//    });
-//    console.log("Creating (POSTING)...!");
-  //  console.log(this.newPerson);
-//  },
-//  newPersonData() {
-  //  return {
-  //    certifyingAgency: "",
-    //  certificationName: "",
-  //    standardExpiry: "",
-  //  }
-//  }
-//},
+  }
+
 
 },
 created() {
