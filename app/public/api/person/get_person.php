8f813357c66b8687d7f3efe449e396466a1ebe71
bradscot <bradenscot13@gmail.com>
2020-10-15 23:47:06 -0400

    added common php to each file

--- app/public/api/person/get_person.php
+++ app/public/api/person/get_person.php
@@ -8,17 +8,13 @@ $db = DbConnection::getConnection();
 $sql = 'SELECT * FROM Person';
 $vars = [];
 
-if (isset($_GET['PersonalID'])) {
-  $sql = 'SELECT * FROM Person WHERE PersonalID = ?';
-  $vars = [$_GET['PersonalID']]
-}
 
-$stmt = $db->prepare($sql)
+$stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
 $users = $stmt->fetchAll();
 
-$json = json_encode($users, JSON_PRETTY_PRINT)
+$json = json_encode($users, JSON_PRETTY_PRINT);
 
 header('Content-Type: application/json');
 echo $json;
