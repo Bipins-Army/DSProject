825cb6ee10bf126eb6b9182900ef2b1518072b2e
bradscot <bradenscot13@gmail.com>
2020-10-16 10:14:22 -0400

    long edit to stringify on vue js post function

--- app/public/js/person.app.js
+++ app/public/js/person.app.js
@@ -47,7 +47,7 @@ var app = new Vue({
       });
     },
     createPerson(){
-      this.newPerson.PersonalID = (this.newPerson.firstName.substring(0,1,2,3,4,5,6,7,8,9,10,11,12)+this.newPerson.lastName+this.newPerson.street).toLowerCase();
+      this.newPerson.PersonalID = (this.newPerson.firstName.substring(0,1,2,3,4,5,6,7,8,9,10,11,12)+this.newPerson.lastName+this.newPerson.street+this.newPerson.city+this.newPerson.zipcode+this.newPerson.email+this.newPerson.dateofBirth+this.newPerson.startDate+this.newPerson.gender+this.newPerson.postion+this.newPerson.radioNumber+this.newPerson.stationNumber+this.newPerson.isActive).toLowerCase();
       fetch('api/person/create_person.php',{
       method:'POST',
       body: JSON.stringify(this.newPerson),
