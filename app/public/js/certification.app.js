63da48f91fc6329a4801ab42fbc4ed91adce30aa
Luffygo <2665034268@qq.com>
2020-10-19 16:02:14 -0400

    format change

--- app/public/js/certification.app.js
+++ app/public/js/certification.app.js
@@ -1,7 +1,6 @@
 var app = new Vue({
   el: '#certificationPage',
   data: {
-<<<<<<< HEAD
     certification: [{
       CertificationID:'',
       certifyingAgency: '',
@@ -14,23 +13,6 @@ var app = new Vue({
       certificationName: '',
       standardExpiry: ''
     }
-=======
-    certification: {
-      CertificationID:"",
-      certifyingAgency: "",
-      certificationName: "",
-      standardExpiry: ""
-    },
-    newCertification: {
-      CertificationID:"",
-      certifyingAgency: "",
-      certificationName: "",
-      standardExpiry: ""
-    }
-  },
-  created() {
-    this.fetchUser();
->>>>>>> a49806df0074ee313b9a88b34b5e603c170c61f1
   },
 
   methods: {
@@ -49,15 +31,9 @@ var app = new Vue({
       body: JSON.stringify(this.newCertification),
       headers: {
         "CONTENT_TYPE": "application/json; charset=utf-8"
-<<<<<<< HEAD
       }
     })
     .then( response => response.json())
-=======
-      }}
-    )
-    .then( response => response.json() )
->>>>>>> a49806df0074ee313b9a88b34b5e603c170c61f1
     .then( json => {
       console.log("Returned from post:", json);
       this.certification=json;
@@ -66,7 +42,6 @@ var app = new Vue({
     console.log("Creating (POSTING)...!");
     console.log(this.newCertification);
   },
-<<<<<<< HEAD
   newCertificationData() {
     return {
       CertificationID:"",
@@ -74,14 +49,6 @@ var app = new Vue({
       certificationName: "",
       standardExpiry: ""
     }
-=======
-newCertificationData(){
-  return{
-    CertificationID:"",
-    certifyingAgency: "",
-    certificationName: "",
-    standardExpiry: ""
->>>>>>> a49806df0074ee313b9a88b34b5e603c170c61f1
   }
 }
 
