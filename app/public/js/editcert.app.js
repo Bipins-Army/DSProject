9d9e380068819f16a550edd3f5895915d03216cf
Alexis Yannuzzi <yannuzzi367@gmail.com>
2020-10-31 12:50:26 -0400

    edit cert js file

---
+++ app/public/js/editcert.app.js
@@ -0,0 +1,29 @@
+editcertapp = new Vue ({
+  el: '#editcert',
+  data: {
+    editcert: []
+  },
+  methods: {
+    editCert(evt){
+      fetch('api/Certification/update_cert.php', {
+          method: 'POST',
+          body: JSON.stringify(this.editcert),
+          headers: {
+            "Content-Type": "application/json; charset=utf-8"
+          }
+        })
+    },
+
+    editCertData() {
+      this.editcert={
+        certifyingAgency:'',
+        certificationName:'',
+        standardExpiry:''
+      }
+    }
+
+  },
+  created() {
+    this.editCertData();
+}
+});
