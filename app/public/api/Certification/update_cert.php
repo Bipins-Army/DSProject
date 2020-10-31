c70e463ceac2e5d40b14796baac06f9a962d6963
Alexis Yannuzzi <yannuzzi367@gmail.com>
2020-10-31 14:26:22 -0400

    edit certification works

--- app/public/api/Certification/update_cert.php
+++ app/public/api/Certification/update_cert.php
@@ -7,21 +7,25 @@ $db = DbConnection::getConnection();
 
 
 $stmt = $db->prepare(
-  'UPDATE Certification SET certifyingAgency=?,certificationName=?,standardExpiry=?'
-);
+  'UPDATE Certification SET certifyingAgency=?,certificationName=?,standardExpiry=?
+  WHERE CertificationID =?');
+
 
 $stmt->execute([
   $_POST['certifyingAgency'],
   $_POST['certificationName'],
-  $_POST['standardExpiry']
+  $_POST['standardExpiry'],
+  $_POST['CertificationID']
 ]);
 
 // If needed, get auto-generated PK from DB
-$pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
+// $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
+
+$certification_id = $_POST['CertificationID'];
+//
 header('HTTP/1.1 303 See Other');
-header('Content-Type: application/json');
-header('Location: ../Certification/?CertificationID=' . $pk);
+header('Location: ../Certification/?CertificationID=' .$certification_id);
