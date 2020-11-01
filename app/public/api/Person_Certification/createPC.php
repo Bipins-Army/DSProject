2253b6b897ff2a66bbb5306119588c6cc694809e
bradscot <bradenscot13@gmail.com>
2020-11-01 14:54:34 -0500

    added variable to store id for php file

--- app/public/api/Person_Certification/createPC.php
+++ app/public/api/Person_Certification/createPC.php
@@ -22,13 +22,13 @@ $stmt->execute([
   $_POST['expirationDate']
 ]);
 
-$id = $_POST['CertificationID']
+//$id = $_POST['CertificationID']
 // If needed, get auto-generated PK from DB
 // $pk = $db->lastInsertId();  // https://www.php.net/manual/en/pdo.lastinsertid.php
 
 // Step 4: Output
 // Here, instead of giving output, I'm redirecting to the SELECT API,
 // just in case the data changed by entering it
-header('HTTP/1.1 303 See Other');
-header('Content-Type: application/json');
-header('Location: ../Person_Certification/?CertificationID=' .$id);
+//header('HTTP/1.1 303 See Other');
+//header('Content-Type: application/json');
+//header('Location: ../Person_Certification/?CertificationID=' .$id);
