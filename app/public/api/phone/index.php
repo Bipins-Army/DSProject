9ccc662becd77af3975975ffe6d0e5b9dad26dd6
bradscot <bradenscot13@gmail.com>
2020-11-02 12:46:51 -0500

    fixed phone page

--- app/public/api/phone/index.php
+++ app/public/api/phone/index.php
@@ -7,11 +7,7 @@ $db = DbConnection::getConnection();
 $sql = 'SELECT * FROM person_phone';
 $vars = [];
 
-if (isset($_GET['PersonalID'])) {
-  // This is an example of a parameterized query
-  $sql = 'SELECT * FROM person_phone WHERE PersonalID = ?';
-  $vars = [ $_GET['PersonalID'] ];
-}
+
 $stmt = $db->prepare($sql);
 $stmt->execute($vars);
 
